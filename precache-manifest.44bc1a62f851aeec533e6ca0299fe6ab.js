ad80496657e228e53d6523fa062ff1362731b84c
byenet <nhathuynguyenho@gmail.com>
2020-09-06 13:18:27 -0700

    Updates

---
+++ precache-manifest.44bc1a62f851aeec533e6ca0299fe6ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b345750a4de12bdfe1d48850bbf2e902",
+    "url": "/GameOanTuXi_ReactRedux/index.html"
+  },
+  {
+    "revision": "812944dddf232cf7782f",
+    "url": "/GameOanTuXi_ReactRedux/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8a765c94049ce560d8ba",
+    "url": "/GameOanTuXi_ReactRedux/static/css/main.9a4220c3.chunk.css"
+  },
+  {
+    "revision": "812944dddf232cf7782f",
+    "url": "/GameOanTuXi_ReactRedux/static/js/2.ca09037b.chunk.js"
+  },
+  {
+    "revision": "a7b079a06d99e0bdddae2cf2b771ca7a",
+    "url": "/GameOanTuXi_ReactRedux/static/js/2.ca09037b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a765c94049ce560d8ba",
+    "url": "/GameOanTuXi_ReactRedux/static/js/main.b8017a37.chunk.js"
+  },
+  {
+    "revision": "895de4141673e5d7e1b2",
+    "url": "/GameOanTuXi_ReactRedux/static/js/runtime-main.5b80a3b4.js"
+  },
+  {
+    "revision": "1716f3f87bf9af14a763249e77f202da",
+    "url": "/GameOanTuXi_ReactRedux/static/media/Pony.1716f3f8.ttf"
+  },
+  {
+    "revision": "f0bb0ef5a660c01da834ab4b72c16eb6",
+    "url": "/GameOanTuXi_ReactRedux/static/media/bgGame.f0bb0ef5.png"
+  }
+]);
